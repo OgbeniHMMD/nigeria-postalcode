ccbcbfee7d686c5c83fe04cf8f2943c7fca8f4a3
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-05-05 22:39:04 +0100

    Added a dummy 'district' option

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,17 @@
     <div class="container m-2">
       <div>
         <div
-          class="border border-white text-white h4 p-4 mb-4"
+          class="border border-white text-white rounded h4 p-4 mb-4"
           v-html="alert ? alert : 'Select Your State'"
         ></div>
 
-        <div class="form-row p-0">
-          <div class="col-12 col-md-6 mb-3">
+        <div class="form-row p -5">
+          <div class="col-12 col-md-5 mb-3">
             <select
               required
-              id="state"
               v-model="selectedState"
               @change="(alert = 'Select Your Local Govt. Area')"
-              class="form-control form-control-lg border-outline-light rounded-0"
+              class="form-control form-control-lg border-outline-light"
             >
               <option value disabled selected hidden>Select State</option>
               <template v-for="(states, index) in nigeria">
@@ -23,15 +22,14 @@
             </select>
           </div>
 
-          <div class="col-12 col-md-6 mb-3">
+          <div class="col-12 col-md-4 mb-4">
             <select
-              id="lga"
               required
               v-model="postalCode"
-              class="form-control form-control-lg border-light rounded-0"
+              class="form-control form-control-lg border-light"
               @change="(alert = `Your Postal Code Is: <strong>${postalCode}</strong>`)"
             >
-              <option value disabled selected hidden>Select L.G.A</option>
+              <option value disabled selected hidden>Select Local Govt. Area</option>
               <option
                 :key="index"
                 :value="lga.postalCode"
@@ -39,7 +37,17 @@
               >{{lga.name}}</option>
             </select>
           </div>
+
+          <div class="col-12 col-md-3 mb-3">
+            <select disabled required class="form-control form-control-lg border-light">
+              <option value disabled selected hidden>Select District</option>
+            </select>
+          </div>
         </div>
+      </div>
+
+      <div class="pt-4 font-weight-bold">
+        <a href="//github.com/OgbeniHMMD/nigeria-postalcode">Version 0.0.2 (dev)</a>
       </div>
     </div>
   </div>
@@ -63,15 +71,18 @@ export default {
 
 
 <style>
-.alert,
-.border,
-.form-control {
-  border-width: 2px !important;
-}
-
 body {
   background-color: #008751;
   background-image: url("/gradient-squares.png");
   /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}
+a {
+  color: white;
+}
+
+.alert,
+.border,
+.form-control {
+  border-width: 2px !important;
 }
 </style>
